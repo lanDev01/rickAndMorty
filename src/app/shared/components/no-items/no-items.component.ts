a489639aa8abf0d52de65a7283c95aeecc02ac09
LanDev01 <alnjr0001@outlook.com>
2025-01-27 18:14:44 -0300

    Feat: create component no-items

---
+++ src/app/shared/components/no-items/no-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-items',
+  imports: [],
+  templateUrl: './no-items.component.html',
+  styleUrl: './no-items.component.scss'
+})
+export class NoItemsComponent {
+
+}
