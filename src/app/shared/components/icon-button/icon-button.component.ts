0ee2242c5f2e8a7dcc6015481b5d132b428b347f
LanDev01 <alnjr0001@outlook.com>
2025-01-27 10:06:43 -0300

    Feat: add dynamic pagination

--- src/app/shared/components/icon-button/icon-button.component.ts
+++ src/app/shared/components/icon-button/icon-button.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from "@angular/common";
 import { Component, input } from "@angular/core";
 import { LucideAngularModule, ChevronRight } from "lucide-angular";
 
 @Component({
   selector: "app-icon-button",
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, CommonModule],
   templateUrl: "./icon-button.component.html",
   styleUrl: "./icon-button.component.scss",
 })
@@ -11,4 +12,5 @@ export class IconButtonComponent {
   readonly chevronRight = ChevronRight;
 
   disabled = input(false);
+  isActive = input(false);
 }
--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -41,6 +41,8 @@ export class PaginationComponent {
     });
   }
 
+  /////////////////////// pagination /////////////////////////
+
   goToPage(newPage: number): void {
     if (newPage >= 1 && newPage <= this.totalPages()) {
       this.page = newPage;
@@ -71,5 +73,44 @@ export class PaginationComponent {
 
   goToPreviousPage(): void {
     this.goToPage(this.page - 1);
+  }
+
+  /////////////////////// dynamic pagination /////////////////////////
+
+  goToNextRange(): void {
+    const visiblePages = 4;
+    const nextPage = Math.min(this.page + visiblePages, this.totalPages());
+    this.goToPage(nextPage);
+  }
+
+  goToPreviousRange(): void {
+    const visiblePages = 4;
+    const previousPage = Math.max(this.page - visiblePages, 1);
+    this.goToPage(previousPage);
+  }
+
+  getDisplayedPages(): number[] {
+    const total = this.totalPages();
+    const visiblePages = 4; // Number of visible pages
+    const pages: number[] = [];
+
+    const start = Math.max(1, this.page - Math.floor(visiblePages / 2));
+    const end = Math.min(total, start + visiblePages - 1);
+
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+
+    return pages;
+  }
+
+  showLeftEllipsis(): boolean {
+    const visiblePages = 4;
+    return this.page > visiblePages;
+  }
+
+  showRightEllipsis(): boolean {
+    const visiblePages = 4;
+    return this.page + visiblePages - 1 < this.totalPages();
   }
 }
