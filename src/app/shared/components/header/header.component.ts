d2f3cb22a0dcff5625d4639666856962abd9a221
LanDev01 <alnjr0001@outlook.com>
2025-01-27 21:22:37 -0300

    Feat: logic for burger menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,21 @@
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { ThemeToggleComponent } from "../theme-toggle/theme-toggle.component";
+import { LucideAngularModule, X, Menu } from "lucide-angular";
 
 @Component({
   selector: "app-header",
-  imports: [RouterLink, ThemeToggleComponent],
+  imports: [RouterLink, ThemeToggleComponent, LucideAngularModule],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.scss",
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  readonly x = X;
+  readonly menu = Menu;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
